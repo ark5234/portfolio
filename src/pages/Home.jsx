72f9a6ae8ab3a12db5f19557c772ac714fc7b6dd
Vikrant Kawadkar <114129298+ark5234@users.noreply.github.com>
2025-05-23 23:15:36 +0530

    completed ThemeToggel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import { ThemeToggle } from "../components/ThemeToggel";
+
 export const Home = () => {
-    return <div className="min-h-screen"> 
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
         
         {/* Theme Toggle */}
+        <ThemeToggle />
 
         {/* Background Effect */}
 
